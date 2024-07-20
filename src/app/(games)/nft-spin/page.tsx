4125fb7f20322b8f1d755e1b406bc28e9f2175a7
Arup Basak <email.arupbasak@gmail.com>
2024-07-21 01:33:20 +0530

    added NFT Spin UI

--- src/app/(games)/nft-spin/page.tsx
+++ src/app/(games)/nft-spin/page.tsx
@@ -1,30 +1,39 @@
 "use client";
 
-import React from "react";
+import React, { useRef } from "react";
 import Game from "@/components/Game";
 import { useToast } from "@/components/basic/Toast";
 import Main from "@/components/basic/Main";
+import NFTSpin, { NFTSpinRef } from "@/components/game/NFTSpin/NFTSpin";
 
-const GAME_ID = 'nft_spin';
+const GAME_ID = "nft_spin";
 
-const Page = () => {
+const Page: React.FC = () => {
   const { addToast } = useToast();
+  const ref = useRef<NFTSpinRef>(null);
+
+  const handlePlayClick = () => {
+    ref.current?.handleStart(10);
+  };
 
   return (
     <Main>
       <Game.Root>
         <Game.Sidebar>
-          <button onClick={() => addToast("this success is msg", "success")}>
+          <button onClick={handlePlayClick}>Start Game</button>
+          <button onClick={() => addToast("This is a success message", "success")}>
             Show Success Toast
           </button>
-          <button onClick={() => addToast("this error is msg", "error")}>
+          <button onClick={() => addToast("This is an error message", "error")}>
             Show Error Toast
           </button>
-          <button onClick={() => addToast("this info is msg", "info")}>
+          <button onClick={() => addToast("This is an info message", "info")}>
             Show Info Toast
           </button>
         </Game.Sidebar>
-        <Game.UI>Will Updated Soon</Game.UI>
+        <Game.UI>
+          <NFTSpin ref={ref} />
+        </Game.UI>
       </Game.Root>
     </Main>
   );
