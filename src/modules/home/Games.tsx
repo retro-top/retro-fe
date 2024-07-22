05bceaf171ab041c4a50b8e1e629dd5e4a41c959
Arup Basak <email.arupbasak@gmail.com>
2024-07-23 02:34:33 +0530

    changed games into homescreen grid

--- src/modules/home/Games.tsx
+++ src/modules/home/Games.tsx
@@ -1,31 +1,19 @@
 "use client";
 
 import games from "@/constants/games";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import GameCard from "@/components/GameCard";
-import { useWindowWidth } from "@react-hook/window-size";
-import useEmblaCarousel from "embla-carousel-react";
 
 const Game = () => {
-  const width = useWindowWidth();
-  const [emblaRef] = useEmblaCarousel({
-    loop: true,
-    slidesToScroll: 1,
-    align: "start",
-  });
-
   return (
-    <div className="overflow-auto space-y-2" ref={emblaRef}>
+    <div>
       <h3>Our Games</h3>
-      <div className="embla__container">
+      <div
+        className={`grid gap-3`}
+        style={{ gridTemplateColumns: "repeat(auto-fill, minmax(130px, 1fr))" }}
+      >
         {games.map((game) => (
-          <div
-            key={game.href}
-            className="embla__slide"
-            style={{
-              flex: `0 0 ${20000 / width}%`,
-            }}
-          >
+          <div key={game.href}>
             <GameCard game={game} />
           </div>
         ))}
